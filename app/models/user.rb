db13e63335a9804b7cffb22d7d669b3483c1aef3
Bradley <bmcguigandev@outlook.com>
2020-10-22 12:06:43 +0100

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 12 }
+    validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 20 }
+end
