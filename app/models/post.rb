7441c111fd92a8457cbd24d2b53c03798d0639bb
Bradley <bmcguigandev@outlook.com>
2020-10-22 12:58:41 +0100

    add comment model and relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :body, presence: true, length: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
     validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 12 }
     validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 20 }
 end
